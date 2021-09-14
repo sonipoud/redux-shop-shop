95f2dc0373df2cec5a61e29ead547e0fe60d8885
Sonika <sonikapoudyal5@gmail.com>
2021-09-14 18:47:29 -0400

    used redux in cart/index

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,12 +8,19 @@ import { idbPromise } from "../../utils/helpers";
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  })
+
+  const dispatch = useDispatch();
+
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
